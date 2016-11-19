bea032ce830a75ae78f729b702ff457ac80f526b
Tim1520 <timdegerness@gmail.com>
2016-11-19 02:29:03 -0800

    left autonomous finished, flipped for right side

--- 5327C_Autonomous.h
+++ 5327C_Autonomous.h
@@ -6,9 +6,9 @@ void autonCloseRedAlt()
 	gyroSetupAlt(900,127);
 
 	//*Reverse to knock off fence.
-	turn(210,80,LEFT,100);
-	bwds(345,100,100);
-	turn(150,90,RIGHT,100);
+	turn(230,80,LEFT,100);
+	bwds(300,100,100);
+	turn(230,90,RIGHT,100);
 	bwds(140,100,300);
 
 	fwds(130,100,200);
@@ -70,31 +70,32 @@ void autonFarRed()
 	gyroSetup(900,127);
 
 	//*Reverse to knock off fence.
-	turn(245,80,RIGHT,100);
-	bwds(550,100,100);
-	turn(170,90,LEFT,100);
-	bwds(510,100,300);
+	turn(210,80,RIGHT,100);
+	bwds(345,100,100);
+	turn(130,90,LEFT,100);
+	bwds(140,100,300);
 
-	//*Turn and grab the cube.
-	fwds(310,90,50);
-	swing(810,90,RIGHT,50);
-	lift(1,0,0,0,20,4000,20);
-	fwds(1400,80,30);
-	claw(0);
-	lift(0,0,0,0,0,4000,20);
+	fwds(130,100,50);
+	swing(840,90,RIGHT,50);
+	fwds(820,80,100);
+	claw(100);
+	pArm(1700,0.07,0);
 
-  //Turn, launch, and knock off fence.
-	turn(870,90,LEFT,0);
-	claw(0);
-	//launchLiftBwds(1300,100,450,100,300);
+	turn(720,80,LEFT,0);
+	base(-50,50);
+	wait1Msec(200);
+	base(0,0);
+	knockknock(1250,100);
+	bwdsLaunch(250,100,0);
 
-	//*Grab 2 more stars and launch.
-	turn(30,90,LEFT,20);
-	fwds(910,90,300);
-	claw(200);
-	bwds(280,100,300);
-	claw(500);
-	//launch(1);
+	turn(20,80,LEFT,100);
+	fwds(350,100,200);
+	claw(400);
+	pArm(1400,0.07,0);
+	lift(1,0,0,0,20,4000,0);
+	bwds(100,100,0);
+	bwdsLaunch(400,100,0);
+
 	//*/
 	wait1Msec(1000);
 }
